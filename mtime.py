aa91d2a801093b5c91960fb5425edc27915a9de1
bg <bg@example.com>
2017-04-02 13:17:37 +0800

    fixed bug

--- mtime.py
+++ mtime.py
@@ -5,32 +5,22 @@ import sendMail
 import time
 import datetime
 
-class xs:
-    def __init__(self):
-        self.__new = ''
-
-    def update(self, name):
-        self.__new = name
-
-    def isNew(self, name):
-        return self.__new != name
-
-    def newCharp(self):
-        return self.__new
-
-def checkToday(timeWork):  #
+def checkToday():  #
     url = 'http://movie.mtime.com/recent/#hottest'
     html = requests.get(url)
     selector = etree.HTML(html.text)
     content_field = selector.xpath('//div[@class="movietopmod"]')
-    num = 10
+    num = 20
     for each in content_field:
 
         syrq = each.xpath('div[@class="txtbox"]/p[@class="showday"]//text()')[0]
-        dq = each.xpath('div[@class="txtbox"]/p[3]/a/text()')[0]
+        dq = each.xpath('//p[contains(text(),"国家地区：")]/a/text()')[0]
+        #dq = each.xpath('div[@class="txtbox"]/p[3]/a/text()')[0]
         #print(each.xpath('div[@class="txtbox"]/h3//text()')[0])
         #print(dq)
         syrq = syrq.split(' ')[1].split('(')[0]
+        if syrq[-1] == '年' or syrq[-1] == '月':
+            continue
         t = (time.strptime(syrq, '%Y年%m月%d日'))
         day_time = datetime.date.today()
         oneday = datetime.timedelta(days=1)
@@ -40,22 +30,28 @@ def checkToday(timeWork):  #
         if t <= withinAWeek and dq != '中国':
             jqUrl = each.xpath('div[@class="txtbox"]/h3//@href')[0]
             jqHtml = requests.get(jqUrl + '/plots.html')
+            print(jqUrl + '/plots.html')
             jqSelector = etree.HTML(jqHtml.text)
-            jqTexts = jqSelector.xpath('//div[@id="paragraphRegion"]/div/div')
-            print(jqTexts)
+            jqDivs = jqSelector.xpath('//div[@id="paragraphRegion"]/div/div')
+
             jqText = ''
-            for txt in jqTexts[1:]:
-                jqText += txt.xpath('div[2]/p[1]/text()')[0] + '\n'
+            for txt in jqDivs[1:]:
+                texts = (txt.xpath('div[2]/p'))
+                for textP in texts:
+                    txt = textP.xpath('string(.)')
+                    jqText += txt + '\n'
+                    #print(txt)
             dym = each.xpath('div[@class="txtbox"]/h3//text()')[0]
             print(dym)
             print(jqUrl)
             print(jqText)
             print(syrq)
             sentTxt = '%s\n上映时间%s\n%s\n' %(jqUrl, syrq, jqText)
+            print(sentTxt)
             sendMail.sendMail('电影：'+dym, sentTxt)
 
         num -= 1
         if num < 0:
             break
 
-checkToday(timeWork)
+checkToday()
